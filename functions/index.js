7dabeb6d3f1c125c4a1d30687ce87715d7407453
Uday Garg <u.garg14@gmail.com>
2021-03-04 17:12:16 +0530

    implemeneted functionality, Delete profile image of user from the storage,Make delete account feature chat app,Make circular progress indicator on pages which takes time to load

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,24 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-const client = require('firebase-tools');
+const firebase_tools = require('firebase-tools');
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
 
    exports.addMessage = functions.https.onRequest(async (req, res) => {
      // Grab the text parameter.
 
      const original = req.body.data.text;
      console.log(original);
-//     if (!(typeof original === 'string') || original.length === 0) {
-//       // Throwing an HttpsError so that the client gets the error details.
-//       throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
-//           'one arguments "text" containing the message text to add.');
-//     }
-  await client.firestore
-            .delete("/message/${original}", {
-               project: process.env.GCLOUD_PROJECT,
-             recursive: true,
-             yes: true
-             });
+
+
      // Push the new message into Firestore using the Firebase Admin SDK.
-     const deleteUsers = await admin.firestore().collection('users').doc(original).delete().then(() => {
-        console.log("Document successfully deleted");
+//     const deleteUsers = await admin.firestore().collection('users').doc(original).delete().then(() => {
+//        console.log("Document successfully deleted");
+//
+//     }).catch((err) =>{
+//        console.error(err);
+//         });
 
-     }).catch((err) =>{
-        console.error(err);
-         });
-
-//     const deleteMessages = await admin.firestore().collection('message').doc(original).delete().then(() => {
-//                   console.log("Document successfully deleted");
-//         }).catch((err) =>{
-//                   console.error(err);
-//                   })
 
       const user = await admin.auth().deleteUser(original).then(()=>{
       console.log("user successfully deleted");
@@ -46,12 +26,36 @@ admin.initializeApp();
       console.error(err);
       });
 
-      // delete sub collection
-
-
-
-
-
      // Send back a message that we've successfully written the message
                 res.json({result: `User deleted`});
    });
+
+   exports.recursiveDelete = functions
+     .runWith({
+       timeoutSeconds: 540,
+       memory: '2GB'
+     })
+     .https.onCall(async (data, context) => {
+       // Only allow admin users to execute this function.
+
+
+       const path = data.path;
+       console.log(
+         `User ${context.auth.uid} has requested to delete path ${path}`
+       );
+
+       // Run a recursive delete on the given document or collection path.
+       // The 'token' must be set in the functions config, and can be generated
+       // at the command line by running 'firebase login:ci'.
+       await firebase_tools.firestore
+         .delete(path, {
+           project: process.env.GCLOUD_PROJECT,
+           recursive: true,
+           yes: true,
+
+         });
+
+       return {
+         path: path
+       };
+     });
