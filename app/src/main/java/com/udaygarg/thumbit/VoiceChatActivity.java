5eda5cde2fd248a10fcb422c547116b59efb7135
Uday Garg <u.garg14@gmail.com>
2021-03-15 20:08:43 +0530

    audio voice chatroom functionality implemented with agora

---
+++ app/src/main/java/com/udaygarg/thumbit/VoiceChatActivity.java
@@ -0,0 +1,14 @@
+package com.udaygarg.thumbit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoiceChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_chat);
+    }
+}
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,13 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const firebase_tools = require('firebase-tools');
+const express = require('express');
+const {RtcTokenBuilder, RtcRole} = require('agora-access-token');
+
+const PORT = 8080;
+const APP_ID = process.env.APP_ID;
+const APP_CERTIFICATE = process.env.APP_CERTIFICATE;
+
 admin.initializeApp();
 
 
@@ -11,14 +18,6 @@ admin.initializeApp();
      console.log(original);
 
 
-     // Push the new message into Firestore using the Firebase Admin SDK.
-//     const deleteUsers = await admin.firestore().collection('users').doc(original).delete().then(() => {
-//        console.log("Document successfully deleted");
-//
-//     }).catch((err) =>{
-//        console.error(err);
-//         });
-
 
       const user = await admin.auth().deleteUser(original).then(()=>{
       console.log("user successfully deleted");
@@ -26,7 +25,6 @@ admin.initializeApp();
       console.error(err);
       });
 
-     // Send back a message that we've successfully written the message
                 res.json({result: `User deleted`});
    });
 
@@ -61,56 +59,6 @@ admin.initializeApp();
      });
 
 
-
-//     exports.onCreateActivityFeedItem = functions.firestore
-//       .document("/messageNotification/{userId}/message/{messageId}")
-//       .onCreate(async (snapshot, context) => {
-//         console.log("Activity feed item created", snapshot.data());
-//
-//         //1) get the user connected to the feed
-//
-//         const userId = context.params.userId;
-//         const userRef = admin.firestore().doc(`users/${userId}`);
-//
-//         const doc = await userRef.get();
-//
-//         //2 once we have the user, check if they have notification token, send notification if they have a token
-//         const androidNotificationToken = doc.data().androidNotificationToken;
-//         console.log(androidNotificationToken);
-//         const createdActivityFeedItem = snapshot.data();
-//         if (androidNotificationToken) {
-//           //send notification
-//           sendNotification(androidNotificationToken, createdActivityFeedItem);
-//         } else {
-//           console.log("No token for user, cannot user notification");
-//         }
-//
-//         function sendNotification(androidNotificationToken, activityFeedItem) {
-//           let body;
-//           //switch body value based off of notification type
-//           body = `${activityFeedItem.message}`;
-//
-//           }
-//           //create message for push notifications
-//           const message = {
-//             notification: { body },
-//             token: androidNotificationToken,
-//             data: { recipient: userId },
-//           };
-//
-//           //send message with admin.messaging()
-//           admin
-//             .messaging()
-//             .send(message)
-//             .then((response) => {
-//               console.log("Successfully sent message", response);
-//             })
-//             .catch((err) => {
-//               console.log("Error sending message", err);
-//             });
-//         }
-//
-//       });
 
 
        exports.onCreateActivityFeedItem = functions.firestore
@@ -163,4 +111,53 @@ admin.initializeApp();
                         });
                     }
            });
+
+
+    const app = express();
+
+
+
+    const nocache = (req, resp, next) => {
+     resp.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+     resp.header('Expires', '-1');
+     resp.header('Pragma', 'no-cache');
+     next();
+    }
+
+
+    exports.generateToken = functions.https.onRequest(async(req, resp) => {
+     // set response header
+     resp.header('Acess-Control-Allow-Origin', '*');
+     // get channel name
+     const channelName = req.body.data.channelName;
+     if (!channelName) {
+       return resp.status(500).json({ 'error': 'channel is required' });
+     }
+     // get uid
+//     let uid = req.query.uid;
+//     if(!uid || uid == '') {
+//       uid = 0;
+//     }
+     uid=0;
+     // get role
+     let role = RtcRole.SUBSCRIBER;
+     if (req.query.role == 'publisher') {
+       role = RtcRole.PUBLISHER;
+     }
+     // get the expire time
+     let expireTime = req.query.expireTime;
+     if (!expireTime || expireTime == '') {
+       expireTime = 3600;
+     } else {
+       expireTime = parseInt(expireTime, 10);
+     }
+     // calculate privilege expire time
+     const currentTime = Math.floor(Date.now() / 1000);
+     const privilegeExpireTime = currentTime + expireTime;
+     // build the token
+     const token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
+     // return the token
+     return resp.json({ 'token': token });
+    });
+
 
